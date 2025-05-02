63e7e5273625bc36569da07e2441afdb54dfa3a2
Andrejs Fedjajevs <fluentnumbers@gmail.com>
2025-05-02 10:42:43 +0200

    Quartz sync: May 2, 2025, 10:42 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -60,8 +60,8 @@ const config: QuartzConfig = {
       }),
       Plugin.SyntaxHighlighting({
         theme: {
-          light: "github-light",
           dark: "github-dark",
+          light: "github-light",
         },
         keepBackground: false,
       }),
